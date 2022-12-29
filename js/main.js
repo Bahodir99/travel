2cdd01e2bfeb2a19c4fc13b29b8a5e3e97933bbe
Bahodir_99 <bahodirataboyev7@gmail.com>
2022-12-30 00:58:42 +0500

    with animation

--- js/main.js
+++ js/main.js
@@ -11,13 +11,13 @@ var sum = prompt('pulingizni kiriting:$ ');
 if (sum * dollor1 >= 8250000){
     console.log('safaringiz behatar bolsin');
     
-    elPrice.textContent = 'Sizda sayr qilish imkoni mavjud!'
+    elPrice.textContent = 'Sizda sayohat qilish imkoni mavjud!'
 }
 
 else{
     console.log('boshqa safar borasiz');
 
-    elError.textContent = 'sizda yetarlicha mablag` yoq!'
+    elError.textContent = 'sizda sayohat uchun yetarlicha mablag` yoq!'
 }
 
 
@@ -30,11 +30,11 @@ var elErrorwalk = document.querySelector('.errorwalk')
 if (walk * euro >= 1560000){
     console.log('siz bemalol sayr qilishingiz mumkin');
     var elWalk = document.querySelector('.walk');
-    elWalk.textContent = 'sizda pul yetarli!'
+    elWalk.textContent = 'sizda sayir uchun pul yetarli!'
 }
 
 else{
-    console.log('sizda yetarlicha mablag` yoq');
+    console.log('sizda sayr etish uchun yetarlicha mablag` yoq');
 
     elErrorwalk.textContent = 'siz sayir qila olmaysiz!'
 }
